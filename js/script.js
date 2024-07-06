f3e4500b3da2951963f31623799892e88194cb8f
Milos <92920530+milos970@users.noreply.github.com>
2024-07-06 13:33:56 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -62,8 +62,8 @@ mapSlovak.set("description6", "");
 mapSlovak.set("title7", "Softvérový nástroj na výučbu predmetu Numerické metódy");
 mapSlovak.set(
   "description7",
-  "Web aplikácia, ktorej primárnym 
-cieľom je spestrenie a zefektívnenie práce študentov a vyučujúceho na predmete Numerické metódy"
+  "Web aplikácia, ktorej primárnym" + 
+"cieľom je spestrenie a zefektívnenie práce študentov a vyučujúceho na predmete Numerické metódy"
 );
 
 const mapEnglish = new Map();
@@ -102,5 +102,5 @@ mapEnglish.set("title6", "Algorithms");
 mapEnglish.set("description6", "");
 
 mapEnglish.set("title7", "Software tool for teaching the subject of Numerical methods");
-mapEnglish.set("description7", "Web software 
-serving exclusively as a teaching aid for students of the Numerical Methods subject");
+mapEnglish.set("description7", "Web software" + 
+"serving exclusively as a teaching aid for students of the Numerical Methods subject");
