f19aad4331bd0bd909ceae258f261a0c59395fa7
Milos <92920530+milos970@users.noreply.github.com>
2025-06-16 11:33:41 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -61,7 +61,11 @@ mapSlovak.set(
 mapSlovak.set("title6", "Specific task manager (v príprave)");
 mapSlovak.set("description6", "Správca úloh prispôsobený mojim potrebám, keďže väčšina bezplatných softvérových alternatív nie je pre moje účely vhodná. Tento softvér bude ideálny pre každého, koho osobný rastový ekosystém je podobný môjmu.");
 
-
+mapSlovak.set("title8", "Refaktorovanie aplikácie Softvérový nástroj na výučbu predmetu Numerické metódy (v príprave)");
+mapSlovak.set("description8", "V rámci budúceho nasadenia aplikácie na školský server, bude backend navrhnutý podľa architektúry MVC a funkcionality budú prístupné prostredníctvom REST API." +
+              "Frontend Implementácia numerických metód sa presunie na serverovú stranu, čím sa dosiahne vyššia výkonnosť a bezpečnosť výpočtov." +
+  "Nasadenie aplikácie bude realizované prostredníctvom kontajnerizácie s využitím nástroja Docker." +
+  "Autentifikácia používateľov bude zabezpečená prostredníctvom školského LDAP servera a autorizácia prostredníctvom vstavaného RDBMS H2.");
 
 const mapEnglish = new Map();
 mapEnglish.set("title7", "Software tool for teaching the subject of Numerical methods");
@@ -102,4 +106,8 @@ mapEnglish.set("title6", "Specific task manager (ongoing)");
 mapEnglish.set("description6", "Task manager tailor-made to my needs, as most free software alternatives are unsuitable for my purposes. This software will be ideal for anyone whose personal growth ecosystem is similar to mine.");
 
 
-
+mapEnglish.set("title8", "Refactoring the Application “Software Tool for Teaching the Course Numerical Methods” (in preparation)");
+mapEnglish.set("description8", "As part of the future deployment of the application on the school server, the backend will be designed according to the MVC architecture, and functionality will be accessible through a REST API." +
+"The implementation of numerical methods will be moved to the server side, improving performance and computation security." +
+"The application will be deployed using containerization with the Docker tool." +
+"User authentication will be handled via the school’s LDAP server, and authorization will be managed through the built-in H2 relational database system.");
